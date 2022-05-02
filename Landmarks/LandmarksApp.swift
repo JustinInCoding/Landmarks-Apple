845569fe0ac72f36a8430a68fa63f9d557b4ffde
Justin <justin.wang4@icloud.com>
2022-05-02 11:00:12 +0800

    [init] project
    init project

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Justin on 2022/5/2.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
